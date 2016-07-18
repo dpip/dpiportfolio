fed4943f24ab64eea084158cd4f741bdfa7863fb
Daniel Pipkin <dpip.nc@gmail.com>
2016-07-18 13:33:00 -0400

    window locations redirect / hover stylings applied / minor media query work

--- assets/js/about.js
+++ assets/js/about.js
@@ -21,4 +21,49 @@ dpip.controller( 'AboutController', function ($scope) {
 
     });
 
+
+
+
+    $('#m1').hover(function() {
+        $(this).toggleClass('git-hover');
+      });
+    $('#m2').hover(function() {
+        $(this).toggleClass('in-hover');
+      });
+    $('#m3').hover(function() {
+        $(this).toggleClass('tweet-hover');
+      });
+    $('#m4').hover(function() {
+        $(this).toggleClass('eightTracks-hover');
+      });
+    $('#m5').hover(function() {
+        $(this).toggleClass('insta-hover');
+      });
+
+      var git = "https://github.com/dpip";
+      var linkedIn = "https://www.linkedin.com/in/daniel-pipkin-a77555116";
+      var twitter = "https://twitter.com/pipmoffett";
+      var eightTracks = "http://8tracks.com/mix_sets/liked:1044774/1";
+      var instagram = "https://www.instagram.com/dpipnc/?hl=en";
+
+      $('#m1').click(function() {
+        window.location.href = git ;
+      });
+
+      $('#m2').click(function() {
+        window.location.href = linkedIn ;
+      });
+
+      $('#m3').click(function(){
+        window.location.href = twitter ;
+      })
+
+      $('#m4').click(function(){
+        window.location.href = eightTracks ;
+      })
+
+      $('#m5').click(function(){
+        window.location.href = instagram ;
+      })
+
 });
